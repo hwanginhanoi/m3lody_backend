d77504d751a73d25865741007a573da17aa23b00
Luu Tuan Hoang <hoanglt0104@gmail.com>
2024-02-24 00:20:47 +0700

    Update .env

--- db.ts
+++ db.ts
@@ -1,6 +1,23 @@
-const Pool = require('pg').Pool;
-const pool = new Pool(({
-    user: '',
-    host: '',
-    password:'',
-}))
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const {
+    DATABASE_URL,
+    DATABASE_USERNAME,
+    DATABASE_PASSWORD,
+    DATABASE_PORT,
+    DATABASE
+} = process.env;
+
+// Create a new pool instance
+const pool = new Pool({
+    connectionString: DATABASE_URL,
+    user: DATABASE_USERNAME,
+    password: DATABASE_PASSWORD,
+    port: parseInt(DATABASE_PORT || '5432'),
+    database: DATABASE
+});
+
+export default pool;
