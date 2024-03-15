6445927f76c76c6f28c5cdbbd7b7495b99d21ee6
khanhdnk <121726197+khanhdnk@users.noreply.github.com>
2024-03-16 01:34:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import path from 'path';
-import cookieParser from 'cookie-parser';
+// import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 // import indexRouter from './routes/index';
 // import usersRouter from './routes/users';
@@ -13,8 +13,10 @@ app.use(cors());
 
 app.use(session({
     secret: 'secret',
-    cookie: { maxAge: 60000000 },
-    saveUninitialized: false
+    cookie: false,
+    saveUninitialized: false,
+    resave: false
+
 })
 );
 
@@ -25,15 +27,16 @@ app.use(session({
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 
 // app.use('/index', indexRouter);
 // app.use('/users', usersRouter);
 app.use('/transactions', transactionsRouter);
 app.use('/register', registerRouter );
 app.post('/login', (req, res) => {
+    console.log(req.sessionID);
+    res.send('hello')
     console.log('hello');
-    res.send('hello');
 });
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
