89186b878e0c792c362d9e482a8eba513ecee6d9
khanhdnk <dangnamkhanhkdgv@gmail.com>
2024-03-18 21:09:48 +0700

    u

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use(session({
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 // app.use(cookieParser());
 
 app.use('/transactions', transactionsRouter);
