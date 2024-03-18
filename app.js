6c17dff482ce75535ef71cd4f691c12a8595e27e
khanhdnk <121726197+khanhdnk@users.noreply.github.com>
2024-03-18 23:45:03 +0700

    u

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ app.use(session({
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 // app.use(cookieParser());
 
 app.use('/transactions', transactionsRouter);
