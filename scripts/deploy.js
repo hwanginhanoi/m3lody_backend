2c916179be2024f316b95ae02ab5c4851ed78601
Lê Xuân Nhật <54104903+rt-kc@users.noreply.github.com>
2024-03-22 20:40:57 +0700

    deploy demo

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,16 @@
-import { ethers } from "hardhat";
+import { ethers } from "ethers";
 
 async function main() {
-
+    const contract = require("../artifacts/contracts/migrate.sol/EtherSender.json");
+    let url = "http://localhost:8545";
+    let provider = new ethers.providers.JsonRpcProvider(url);
+    const privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+    const wallet = new ethers.Wallet(privateKey, provider);
+    const account = wallet.connect(provider);
+    const contractFactory = new ethers.ContractFactory(contract.abi, contract.bytecode, account);
+    const contractDeploy = await contractFactory.deploy();
+    await contractDeploy.deployed();
+    console.log("Contract deployed to:", contractDeploy.address);
 }
 
 main()
