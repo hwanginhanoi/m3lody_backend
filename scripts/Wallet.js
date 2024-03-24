3f04123763bb067f6d024953c8eae8a50e5dfc9d
Thanh Duong <clanaoetruongxe@gmail.com>
2024-03-24 17:15:20 +0700

    ec ec

--- scripts/Wallet.js
+++ scripts/Wallet.js
@@ -1,9 +1,8 @@
 // scripts/transferOwnershis.js
 const hre = require('hardhat');
-
 async function main() {
     const [deployer] = await hre.ethers.getSigners();
-
+    const provider = new hre.ethers.providers.Web3Provider(window.Ethereum)
     console.log("Deploying contract with the account:", deployer.address);
 
     const ConfirmationContract = await hre.ethers.getContractFactory("ConfirmationContract");
@@ -13,7 +12,7 @@ async function main() {
     console.log("Contract address:", contract.address);
 
     // Transfer ownership (example)
-    const newOwner = "0xNewOwnerAddress"; // Address of the new owner
+    const newOwner = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"; // Address of the new owner
     console.log("Transferring ownership to:", newOwner);
     await contract.connect(deployer).transferOwnership(newOwner);
 
