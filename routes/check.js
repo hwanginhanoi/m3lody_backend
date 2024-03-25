0a6e65c6f267a4f4d687d57cfe39d7b3b17c9397
Thanh Duong <clanaoetruongxe@gmail.com>
2024-03-25 07:49:24 +0700

    update

--- routes/check.js
+++ routes/check.js
@@ -3,8 +3,8 @@ import { getPurchase } from "../scripts/getFromTxHash.js";
 var router = express.Router();
 //check purchase
 router.post("/", async (req, res) => {
-    let { hash } = req.body;
-    let result = await getPurchase(hash);
+    let { hash, id } = req.body;
+    let result = await getPurchase(hash, id);
     res.status(200).json({
         data: result,
         success: true
--- scripts/getFromTxHash.js
+++ scripts/getFromTxHash.js
@@ -1,45 +1,184 @@
-import { ethers } from "ethers";
-
+import ethers from 'ethers'
+import config from 'dotenv'
 // Provider (Ethereum node endpoint)
 const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
 
 // Transaction Hash
+// const transactionHash = '0xc651caa278b1beb69c8cab2dc6f612a7b6df7a24d7796c557104b3acd207320b';
 
 // Function to get product purchase details by transaction hash
-export async function getPurchase(txHash) {
+export async function getPurchase(transactionHash, id) {
     try {
-        // Get transaction receipt
-        const txReceipt = await provider.getTransactionReceipt(txHash);
+        // Get transaction receipt to retrieve contract address
+        const receipt = await provider.getTransactionReceipt(transactionHash);
 
-        if (!txReceipt) {
-            throw new Error("Transaction not found");
-        }
+        const contractAddress = process.env.CONTRACT_ADDRESS;
 
-        // Parse event logs to find ProductPurchased event
-        const ProductPurchasedEvent = txReceipt.logs.find(log => log.topics[0] === ethers.utils.id("ProductPurchased(uint256,address,uint256)"));
+        // Contract ABI (Interface)
+        const contractABI = [
 
-        if (!ProductPurchasedEvent) {
-            throw new Error("ProductPurchased event not found in the transaction logs");
-        }
+            {
+                "anonymous": false,
+                "inputs": [
+                    {
+                        "indexed": false,
+                        "internalType": "uint256",
+                        "name": "productId",
+                        "type": "uint256"
+                    },
+                    {
+                        "indexed": false,
+                        "internalType": "address",
+                        "name": "buyer",
+                        "type": "address"
+                    },
+                    {
+                        "indexed": false,
+                        "internalType": "uint256",
+                        "name": "amountPaid",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "ProductPurchased",
+                "type": "event"
+            },
+            {
+                "stateMutability": "payable",
+                "type": "fallback"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "productId",
+                        "type": "uint256"
+                    },
+                    {
+                        "internalType": "string",
+                        "name": "_name",
+                        "type": "string"
+                    },
+                    {
+                        "internalType": "string",
+                        "name": "_author",
+                        "type": "string"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "_price",
+                        "type": "uint256"
+                    },
+                    {
+                        "internalType": "address payable",
+                        "name": "_seller",
+                        "type": "address"
+                    }
+                ],
+                "name": "addProduct",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "productId",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "getProduct",
+                "outputs": [
+                    {
+                        "internalType": "string",
+                        "name": "",
+                        "type": "string"
+                    },
+                    {
+                        "internalType": "string",
+                        "name": "",
+                        "type": "string"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "",
+                        "type": "uint256"
+                    },
+                    {
+                        "internalType": "address payable",
+                        "name": "",
+                        "type": "address"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "products",
+                "outputs": [
+                    {
+                        "internalType": "string",
+                        "name": "name",
+                        "type": "string"
+                    },
+                    {
+                        "internalType": "string",
+                        "name": "author",
+                        "type": "string"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "price",
+                        "type": "uint256"
+                    },
+                    {
+                        "internalType": "address payable",
+                        "name": "wallet",
+                        "type": "address"
+                    }
+                ],
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "internalType": "uint256",
+                        "name": "productId",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "purchaseProduct",
+                "outputs": [],
+                "stateMutability": "payable",
+                "type": "function"
+            },
+            {
+                "stateMutability": "payable",
+                "type": "receive"
+            }
+        ];
 
-        // Extract productId from the event
-        const productId = ethers.utils.hexStripZeros(ProductPurchasedEvent.topics[1]);
+        // Initialize Contract Instance with contract address
+        const contract = new ethers.Contract(contractAddress, contractABI, provider);
 
-        // Call getProduct function with productId to retrieve product data
-        const product = await contract.getProduct(productId);
-
-        console.log("Product Name:", product[0]);
-        console.log("Author:", product[1]);
-        console.log("Price:", product[2]);
-        console.log("Seller Wallet:", product[3]);
-
-        return product;
+        // Get product purchase details using contract function
+        const result = await contract.getProduct(id);
+        console.log('Product Name:', result[0]);
+        console.log('Author:', result[1]);
+        console.log('Price:', ethers.utils.formatEther(result[2]), 'ETH');
+        console.log('Seller Address:', result[3]);
     } catch (error) {
-        console.error("Error getting product from transaction hash:", error);
-        throw error;
+        console.error('Error:', error);
     }
-
 }
 
 // Example usage
-
+// getPurchase(transactionHash, 1);
