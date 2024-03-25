e36516ba4fe6dcdfecaa04ef60419d81f49a09fa
khanhdnk <121726197+khanhdnk@users.noreply.github.com>
2024-03-25 22:09:20 +0700

    u

--- routes/marketplace.js
+++ routes/marketplace.js
@@ -2,6 +2,9 @@ import express from "express";
 import pool from "../db.js";
 import { querryAllNFT } from "../controllers/marketplace.js";
 const router = express.Router();
+import multer from "multer";
+const upload = multer();
+const app = express();
 //get all nft
 router.get("/", (req, res) => {
     let user_id = req.session.user.user_id;
@@ -24,5 +27,18 @@ router.get("/:id", (req, res) => {
         querryAllNFT(req, res, query);
     }
 });
+
+// router.get("/search/", upload.none(), (req, res)=>{
+//     let user_id = req.session.user.user_id;
+
+//     if (!user_id) {
+//         res.status(401).json({ msg: "Unauthorized" });
+//         return;
+//     } else {
+//         let formData = req.body;
+//         let query = `SELECT * FROM music WHERE title LIKE '%${formData.title}% AND tag = '${formData.tag}'`;
+//         querryAllNFT(req, res, query);
+//     }
+// })
 
 export default router;
