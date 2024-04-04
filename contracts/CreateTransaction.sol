60bab1a47ea0f17f2ffff3bd958a987052b26393
Luu Tuan Hoang <hoanglt0104@gmail.com>
2024-04-04 18:40:20 +0700

    Updated

--- contracts/CreateTransaction.sol
+++ contracts/CreateTransaction.sol
@@ -1,28 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-contract Ownable {
-    address public owner;
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+contract ProductPurchase is ERC721Enumerable, Ownable {
 
-    constructor() {
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can call this function");
-        _;
-    }
-
-    function transferOwnership(address newOwner) external onlyOwner {
-        require(newOwner != address(0), "Invalid address");
-        emit OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-    }
-}
-
-contract ProductPurchase is Ownable {
     // Struct to represent a product
     struct Product {
         string name;
@@ -36,6 +19,8 @@ contract ProductPurchase is Ownable {
 
     // Event to log purchase
     event ProductPurchased(uint256 productId, address buyer, uint256 amountPaid);
+
+    constructor() ERC721("ProductPurchase", "PP") {}
 
     // Function to add a product
     function addProduct(uint256 productId, string memory _name, string memory _author, uint256 _price, address payable _seller) external onlyOwner {
@@ -55,13 +40,18 @@ contract ProductPurchase is Ownable {
 
         // Transfer Ether to the seller
         product.wallet.transfer(msg.value);
+        // Mint the token to the buyer
+        _mint(msg.sender, productId);
         // Emit an event to log the purchase
         emit ProductPurchased(productId, msg.sender, msg.value);
     }
+
     function getProduct(uint256 productId) external view returns (string memory, string memory, uint256, address payable) {
         Product storage product = products[productId];
         return (product.name, product.author, product.price, product.wallet);
     }
+
     fallback() external payable {}
-    receive() external payable { }
+
+    receive() external payable {}
 }
